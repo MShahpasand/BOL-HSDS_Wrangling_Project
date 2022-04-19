4b34a1223bbe3467758aaa9ec4b37c67bd042b2e
MShahpasand <92695959+MShahpasand@users.noreply.github.com>
2022-04-19 16:06:48 +1000

    Update DCJ_HOMELESSNESS_ServiceUse_Indicator.py

--- Financial_Year_Indicators/DCJ_HOMELESSNESS_ServiceUse_Indicator.py
+++ Financial_Year_Indicators/DCJ_HOMELESSNESS_ServiceUse_Indicator.py
@@ -14,16 +14,36 @@ OutputDir = "../HSDS/DCJ_HOMELESSNESS/"
 
 def read_homelessness_data():
 
-  df_homelessness = pd.read_csv(inputDir + 'homelessness_nsw_1518_1920.csv',usecols=['PPN', 'ReportingPeriod_YTD'],parse_dates=['ReportingPeriod_YTD'], infer_datetime_format=True)
+  df_homelessness = pd.read_csv(inputDir + 'homelessness_nsw_1518_1920.csv',usecols=['ppn', 'Initial_SP_Start','Latest_SP_Finished'],parse_dates=['Initial_SP_Start','Latest_SP_Finished'], infer_datetime_format=True)
+  
+  #drop duplicates
   df_homelessness=df_homelessness.drop_duplicates()
+  
+  #drop cases with negaive lenght of service use
+  df_homelessness=df_homelessness.drop(df_homelessness.loc[(df_homelessness.Latest_SP_Finished- df_admissions.Initial_SP_Start).dt.days<0].index)
+    
   return df_homelessness
 
 
 def aggregate_to_FY(df_homelessness):
 
+  #take the list of financial years between the start and end date of a service episode 
   # financial year is considered from 01-07-CurrentYear to 30-06-NextYear
+  df_homelessness = df_homelessness.merge(df_homelessness.apply(lambda s:pd.date_range(str(s.Initial_SP_Start)[:10], s.Latest_SP_Finished, freq='AS-JUK'),1)
+             .explode()
   #HS stands for Homelessness Service
-  df_homelessness['HS'] = df_homelessness.ReportingPeriod_YTD.dt.to_period('Q-JUN').dt.qyear.apply(lambda x : str(x-1)[2:]+str(x)[2:])
+             .rename('HS')
+             .dt.strftime("%Y")
+             .drop(columns=['Initial_SP_Start','Latest_SP_Finished'],axis=0),
+             left_index=True,
+             right_index=True)
+  
+  #fill nan values with the year from start date 
+  df_homelessness.HS=np.where(df_homelessness.HS.isnull(),df['Initial_SP_Start'].dt.to_period('Y'),df_homelessness.HS)
+  
+  #change format to FY (e.g. 2017 -> 1718)
+  df_homelessness.HS=df_homelessness.HS.apply(lambda s: str(s)[2:]+str(int(str(x))+1)[2:])
+
   df_homelessness=pd.get_dummies(df_homelessness,columns=['HS'])
   
   #define a new naming for financial year columns
@@ -34,7 +54,7 @@ def aggregate_to_FY(df_homelessness):
   df_homelessness=df_homelessness.loc[df_homelessness[usecols]].sum(1)>0
   
   #keep only the columns needed and remove duplicates 
-  usecols=["PPN"] + usecols
+  usecols=["ppn"] + usecols
   df_homelessness=df_homelessness[usecols]
   df_homelessness=df_homelessness.drop_duplicates()
   
