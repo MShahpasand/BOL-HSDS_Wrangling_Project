7b7af107497ded43f83a6dae251c01b43753e993
MShahpasand <92695959+MShahpasand@users.noreply.github.com>
2022-04-19 12:38:49 +1000

    Update DCJ_HOMELESSNESS_ServiceUse_Indicator.py

--- Financial_Year_Indicators/DCJ_HOMELESSNESS_ServiceUse_Indicator.py
+++ Financial_Year_Indicators/DCJ_HOMELESSNESS_ServiceUse_Indicator.py
@@ -1,1 +1,16 @@
-sfsdf
+import dask.dataframe as dd
+import dask.multiprocessing
+from dask.diagnostics import ProgressBar
+import pandas as pd
+import numpy as np
+import tables
+import glob
+import os
+
+#change the <inputDir> as where the csv is located in your drive
+inputDir = "../HSDS/DCJ_HOMELESSNESS/"
+#change the <OutputDir> as where you want to save the indicator list
+OutputDir = "../HSDS/DCJ_HOMELESSNESS/"
+
+def read_homelessness_data():
+  
