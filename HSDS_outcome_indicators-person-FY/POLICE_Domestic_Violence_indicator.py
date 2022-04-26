5c6a08da3efb4370c42e6ca7b232bf1a87253187
MShahpasand <92695959+MShahpasand@users.noreply.github.com>
2022-04-26 11:16:33 +1000

    Update POLICE_Domestic_Violence_indicator.py

--- HSDS_outcome_indicators-person-FY/POLICE_Domestic_Violence_indicator.py
+++ HSDS_outcome_indicators-person-FY/POLICE_Domestic_Violence_indicator.py
@@ -1,46 +1,50 @@
-import dask.dataframe as dd
-import dask.multiprocessing
-from dask.diagnostics import ProgressBar
 import pandas as pd
 import numpy as np
-import tables
-import glob
-import os
+from datetime import datetime, date
 
 #change the <inputDir> as where the csv is located in your drive
 inputDir = "../HSDS/POLICE/"
 #change the <OutputDir> as where you want to save the indicator list
 OutputDir = "../HSDS/POLICE/"
 
+outcome_file_name = "POLICE_Domestic_Violence_person_FY_indicator"
+
+#set the start and end for the range of financial years
+financial_year_start = '1011'
+financial_year_end = '1819'
+
 def read_victims_sensitive_data():
 
-   df_victims_sensitive = pd.read_csv(inputDir + 'victims_sensitive.csv',usecols=['ppn','Event_Reported_date','Incident_Category'],parse_dates=['Event_Reported_date'],infer_datetime_format=True)
-   df_victims_sensitive=df_victims_sensitive.drop_duplicates()
-   df_victims_sensitive=df_victims_sensitive.loc[df_victims_sensitive.Incident_Category=='Dpmestic Violence-No Offence']
-   return df_admissions
+   df_victims_sensitive = pd.read_csv(inputDir + 'victims_sensitive_v2.csv',usecols=['ppn','Event_Reported_date','Incident_Category'],parse_dates=['Event_Reported_date'],infer_datetime_format=True)
+   df_victims_sensitive = df_victims_sensitive.drop_duplicates()
+   df_victims_sensitive = df_victims_sensitive.loc[df_victims_sensitive.Incident_Category =='Domestic Violence-No Offence']
+   
+   return df_victims_sensitive
 
-def aggregate_to_FY(df_victims_sensitive):
+def aggregate_to_person_FY(df_victims_sensitive,financial_year_start,financial_year_end):
 
   # financial year is considered from 01-07-CurrentYear to 30-06-NextYear
   df_victims_sensitive['DV'] = df_victims_sensitive.Event_Reported_date.dt.to_period('Q-JUN').dt.qyear.apply(lambda x : str(x-1)[2:]+str(x)[2:])
-  df_victims_sensitive=pd.get_dummies(df_victims_sensitive,columns=['DV'])
+  df_victims_sensitive = pd.get_dummies(df_victims_sensitive,columns=['DV'])
   
   #define a new naming for new columns
-  years=list(range(11,20))
-  usecols=['DV_'+str(x-1)+str(x) for x in years]
+  years = list(range(int(financial_year_start[2:]),int(financial_year_end[2:])+1))
+  usecols = ['DV_'+str(x-1)+str(x) for x in years]
   
   #keep only the rows with at least one flag for a financial year 
-  df_victims_sensitive=df_victims_sensitive.loc[df_victims_sensitive[usecols]].sum(1)>0
+  df_victims_sensitive = df_victims_sensitive.loc[df_victims_sensitive[usecols].sum(1)>0]
   
   #keep only the columns needed and remove duplicates 
-  usecols=["ppn"] + usecols
-  df_victims_sensitive=df_victims_sensitive[usecols]
-  df_victims_sensitive=df_victims_sensitive.drop_duplicates()
+  usecols = ["ppn"] + usecols
+  df_victims_sensitive = df_victims_sensitive[usecols]
+  df_victims_sensitive = df_victims_sensitive.drop_duplicates()
   
-  df_victims_sensitive.to_csv(OutputDir + 'DV_1011_1819_FY_indicator.csv', index=False)
+  return df_victims_sensitive
   
 
 if __name__ == '__main__':
 
-  df_victims_sensitive=read_victims_sensitive_data()
-  aggregate_to_FY(df_victims_sensitive)
+  df_victims_sensitive = read_victims_sensitive_data()
+  df_victims_sensitive = aggregate_to_person_FY(df_victims_sensitive,financial_year_start,financial_year_end)
+  df_victims_sensitive.to_csv(OutputDir + outcome_file_name + '_'+ financial_year_start + '_' +financial_year_end + '.csv' , index=False)
+
