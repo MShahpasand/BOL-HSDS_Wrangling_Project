eaeff704ce5f0a677ede60cdc4ca8790dc3cc647
MShahpasand <92695959+MShahpasand@users.noreply.github.com>
2022-04-26 11:32:04 +1000

    Update DCJ_JUVENILE_JUSTICE_Admission_Indicator.py

--- HSDS_outcome_indicators-person-FY/DCJ_JUVENILE_JUSTICE_Admission_Indicator.py
+++ HSDS_outcome_indicators-person-FY/DCJ_JUVENILE_JUSTICE_Admission_Indicator.py
@@ -1,46 +1,50 @@
-import dask.dataframe as dd
-import dask.multiprocessing
-from dask.diagnostics import ProgressBar
 import pandas as pd
 import numpy as np
-import tables
-import glob
-import os
+from datetime import datetime, date
 
 #change the <inputDir> as where the csv is located in your drive
 inputDir = "../HSDS/DCJ_JUVENILE_JUSTICE/"
 #change the <OutputDir> as where you want to save the indicator list
 OutputDir = "../HSDS/DCJ_JUVENILE_JUSTICE/"
 
-def read_admission_data():
+outcome_file_name = "DCJ_JUVENILE_JUSTICE_Admission_person_FY_indicator"
 
-   df_admissions = pd.read_csv(inputDir + 'jj_admissions.csv',usecols=['PPN', 'AdmissionDate','DischargeDate'],parse_dates=['AdmissionDate','DischargeDate'], infer_datetime_format=True)
-   df_admissions=df_admissions.drop_duplicates()
-   df_admissions=df_admissions.drop(df_admissions.loc[(df_admissions.DischargeDate- df_admissions.AdmissionDate).dt.days<0].index)
-   return df_admissions
+#set the start and end for the range of financial years
+financial_year_start = '1011'
+financial_year_end = '1819'
 
-def aggregate_to_FY(df_admissions):
+def read_jj_admission_data():
+
+   df_jj_admissions = pd.read_csv(inputDir + 'jj_admissions.csv',usecols=['PPN', 'AdmissionDate','DischargeDate'],parse_dates=['AdmissionDate','DischargeDate'],infer_datetime_format=True)
+   df_jj_admissions = df_jj_admissions.drop_duplicates()
+   df_jj_admissions=df_jj_admissions.drop(df_jj_admissions.loc[(df_jj_admissions.DischargeDate - df_jj_admissions.AdmissionDate).dt.days<0].index)
+   
+   return df_jj_admissions
+
+def aggregate_to_person_FY(df_jj_admissions,financial_year_start,financial_year_end):
 
   # financial year is considered from 01-07-CurrentYear to 30-06-NextYear
-  df_admissions['JJ_Adm'] = df_admissions.AdmissionDate.dt.to_period('Q-JUN').dt.qyear.apply(lambda x : str(x-1)[2:]+str(x)[2:])
-  df_admissions=pd.get_dummies(df_admissions,columns=['JJ_Adm'])
-  
+  df_jj_admissions['JJ_Adm'] = df_jj_admissions.AdmissionDate.dt.to_period('Q-JUN').dt.qyear.apply(lambda x : str(x-1)[2:]+str(x)[2:])
+  df_jj_admissions = pd.get_dummies(df_jj_admissions,columns=['JJ_Adm'])
+  df_jj_admissions = df_jj_admissions.reset_index()
+
   #define a new naming for new columns
-  years=list(range(11,20))
-  usecols=['JJ_Adm_'+str(x-1)+str(x) for x in years]
+  years = list(range(int(financial_year_start[2:]),int(financial_year_end[2:])+1))
+  usecols = ['JJ_Adm'+str(x-1)+str(x) for x in years]
   
   #keep only the rows with at least one flag for a financial year 
-  df_admissions=df_admissions.loc[df_admissions[usecols]].sum(1)>0
+  df_jj_admissions = df_jj_admissions.loc[df_jj_admissions[usecols].sum(1)>0]
   
   #keep only the columns needed and remove duplicates 
-  usecols=["PPN"] + usecols
-  df_admissions=df_admissions[usecols]
-  df_admissions=df_admissions.drop_duplicates()
+  usecols = ["ppn"] + usecols
+  df_jj_admissions = df_jj_admissions[usecols]
+  df_jj_admissions = df_jj_admissions.drop_duplicates()
   
-  df_admissions.to_csv(OutputDir + 'jj_admissions_1011_1819_FY_indicator.csv', index=False)
+  return df_jj_admissions
   
 
 if __name__ == '__main__':
 
-  df_admissions=read_admission_data()
-  aggregate_to_FY(df_admissions)
+  df_jj_admissions = read_jj_admission_data()
+  df_jj_admissions = aggregate_to_person_FY(df_jj_admissions,financial_year_start,financial_year_end)
+  df_jj_admissions.to_csv(OutputDir + outcome_file_name + '_'+ financial_year_start + '_' +financial_year_end + '.csv' , index=False)
