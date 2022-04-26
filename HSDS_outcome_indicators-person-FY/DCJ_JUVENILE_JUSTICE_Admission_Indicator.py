e56da1a8bd12296ccae311ee9e6dd563a5bc836a
MShahpasand <92695959+MShahpasand@users.noreply.github.com>
2022-04-26 12:49:50 +1000

    Update DCJ_JUVENILE_JUSTICE_Admission_Indicator.py

--- HSDS_outcome_indicators-person-FY/DCJ_JUVENILE_JUSTICE_Admission_Indicator.py
+++ HSDS_outcome_indicators-person-FY/DCJ_JUVENILE_JUSTICE_Admission_Indicator.py
@@ -18,15 +18,25 @@ def read_jj_admission_data():
    df_jj_admissions = pd.read_csv(inputDir + 'jj_admissions.csv',usecols=['PPN', 'AdmissionDate','DischargeDate'],parse_dates=['AdmissionDate','DischargeDate'],infer_datetime_format=True)
    df_jj_admissions = df_jj_admissions.drop_duplicates()
    df_jj_admissions=df_jj_admissions.drop(df_jj_admissions.loc[(df_jj_admissions.DischargeDate - df_jj_admissions.AdmissionDate).dt.days<0].index)
-   
+   df_jj_admissions.DischargeDate = np.where(df_jj_admissions.DischargeDate.isnull(),df_jj_admissions.AdmissionDate, df_jj_admissions.DischargeDate)
+
    return df_jj_admissions
 
 def aggregate_to_person_FY(df_jj_admissions,financial_year_start,financial_year_end):
 
   # financial year is considered from 01-07-CurrentYear to 30-06-NextYear
-  df_jj_admissions['JJ_Adm'] = df_jj_admissions.AdmissionDate.dt.to_period('Q-JUN').dt.qyear.apply(lambda x : str(x-1)[2:]+str(x)[2:])
+  df_jj_admissions = df_jj_admissions.merge(df_homelessness.apply(lambda s:pd.date_range(str(s.AdmissionDate)[:10] , s.DischargeDate,freq='AS-JUL'),1)
+                                          .explode()
+                                          .rename('JJ_Adm')
+                                          .drop(columns=['AdmissionDate','DischargeDate'],axis=0)
+                                          lef_index=True,
+                                          right_index=True)
+  
+  df_jj_admissions.JJ_Adm = np.where(df_jj_admissions.JJ_Adm.isnull(),df_jj_admissions.AdmissionDate.dt.to_period('Y'), df_jj_admissions.JJ_Adm)
+  df_jj_admissions.JJ_Adm = df_jj_admissions.JJ_Adm.apply(lambda x: str(x)[2:]+str(int(str(x))+1)[2:])
+
+
   df_jj_admissions = pd.get_dummies(df_jj_admissions,columns=['JJ_Adm'])
-  df_jj_admissions = df_jj_admissions.reset_index()
 
   #define a new naming for new columns
   years = list(range(int(financial_year_start[2:]),int(financial_year_end[2:])+1))
